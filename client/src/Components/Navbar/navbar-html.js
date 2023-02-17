cc86c0ba0c38cf05a0e26ba1d78d7e1fc46066d2
hommieBusiness <contact@hommie.co.in>
2023-02-18 03:04:35 +0530

    New navbar changes

--- client/src/Components/Navbar/navbar-html.js
+++ client/src/Components/Navbar/navbar-html.js
@@ -1,86 +1,75 @@
+import { Fragment } from "react";
 /*
     @Name: renderHTML
     @creator: Aaditya Pal
     @type: Function
     @desc: returns HTML for Navbar component
 */
-export const renderHTML = (classes, Logo, Constants, toggler, navbarToggle,hamBurger) => {
+export const renderHTML = (
+  classes,
+  Logo,
+  Constants,
+  toggler,
+  navbarToggle,
+  hamBurger
+) => {
   return (
-    <div className={classes["navbar-root"]}>
-      <div className={classes["navbar-responsive"]}>
-        <div
-          className={classes["navbar-logo-responsive"]}
-          style={{ backgroundImage: `url(${Logo})` }}
-        ></div>
-        <i
-          ref={hamBurger}
-          className={
-            "fa-sharp fa-solid fa-bars" + " " + classes["navbar-hamBurger"]
-          }
-          onClick={toggler}
-        ></i>
-      </div>
-      <div className={classes["navbar-items"]} ref={navbarToggle}>
+    <Fragment>
+      <div className={classes["navbar-container"]}>
         <div
           className={classes["navbar-logo"]}
           style={{ backgroundImage: `url(${Logo})` }}
         ></div>
-        <div className={classes["navbar-links"]}>
-          <div className={classes["navbar-items-div"]}>
-            <div className={classes["navbar-search"] + " " + "field"}>
-              <p className="control has-icons-left">
-                <input className="input is-small" placeholder="Location" />
-                <span className="icon is-small is-left">
-                  <i className="fa-sharp fa-solid fa-location-dot"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className={classes["navbar-items-div"]}>
-            <p>{Constants.navLinkText1}</p>
-          </div>
-          <div className={classes["navbar-items-div"]}>
-            <p className={classes["navbar-items-text-p"]}>
-              {Constants.navLinkText2}
-            </p>
-            <i className="fa-solid fa-angle-down"></i>
-          </div>
-          <div className={classes["navbar-items-div"]}>
-            <p className={classes["navbar-items-text-p"]}>
-              {Constants.navLinkText3}
-            </p>
-            <i className="fa-solid fa-angle-down"></i>
-          </div>
-          <div className={classes["navbar-items-div"]}>
-            <p className={classes["navbar-items-text-p"]}>
-              {Constants.navLinkText4}
-            </p>
-          </div>
-          <div className={classes["navbar-items-div"]}>
-            <p className={classes["navbar-items-text-p"]}>
-              {Constants.navLinkText5}
-            </p>
-          </div>
-          <div className={classes["navbar-items-div"]}>
-            <p className={classes["navbar-items-text-p"]}>
-              <i className="fa-sharp fa-solid fa-cart-shopping"></i>
-            </p>
-            {Constants.navLinkText6}
-          </div>
-          <div
+        <div className={classes["navbar-items"]}>
+          {navbarLinks(classes, Constants)}
+        </div>
+        <div className={classes["navbar-toggle"]}>
+          <i
+            ref={hamBurger}
             className={
-              classes["navbar-items-text-button"] +
-              " " +
-              classes["navbar-items-div"]
+              "fa-sharp fa-solid fa-bars" + " " + classes["navbar-hamBurger"]
             }
-          >
-            <p className={classes["navbar-items-text-p"]}>
-              {Constants.navLinkText7}
-            </p>
-            <i className="fa-solid fa-angle-down"></i>
-          </div>
+            onClick={toggler}
+          ></i>
         </div>
       </div>
-    </div>
+      {/* This is the div that will be toggled and shown when the screen width is 1000px */}
+      <div className={classes["navbar-responsive-item"]} ref={navbarToggle}>
+        {navbarLinks(classes, Constants)}
+      </div>
+    </Fragment>
+  );
+};
+
+const navbarLinks = (classes, Constants) => {
+  return (
+    <Fragment>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <p>{Constants.navLinkText1}</p>
+      </div>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <p>{Constants.navLinkText2}</p>
+      </div>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <p>{Constants.navLinkText3}</p>
+      </div>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <p>{Constants.navLinkText4}</p>
+      </div>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <p>{Constants.navLinkText5}</p>
+      </div>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <i
+          className="fa-sharp fa-solid fa-cart-shopping"
+          style={{ marginRight: "0.4em" }}
+        ></i>
+        <p>{Constants.navLinkText6}</p>
+      </div>
+      <div className={classes["navbar-responsive-item-div"]}>
+        <i className="fa-solid fa-user" style={{ marginRight: "0.4em" ,color:'var(--color-secondary-1)'}}></i>
+        <p>{Constants.navLinkText7}</p>
+      </div>
+    </Fragment>
   );
 };
