3bfc4108cc2385dfa47a4443482cdceca412c8c6
Amrit Shukla <amritshukla001@gmail.com>
2023-02-17 15:15:56 +0530

    Navbar design complete

---
+++ client/src/Components/Navbar/navbar_constants.js
@@ -0,0 +1,7 @@
+export const navLinkText1 = "timetable";
+export const navLinkText2 = "Meals";
+export const navLinkText3 = "Events";
+export const navLinkText4 = "Commercial";
+export const navLinkText5 = "Customer care";
+export const navLinkText6 = "Cart";
+export const navLinkText7 = "My Account";
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "./Assets/Fonts/static/Montserrat-Black.ttf";
+import "./Assets/Fonts/static/Montserrat-BlackItalic.ttf";
+import "./Assets/Fonts/static/Montserrat-Bold.ttf"; 
+import "./Assets/Fonts/static/Montserrat-BoldItalic.ttf";
+import "./Assets/Fonts/static/Montserrat-ExtraBold.ttf";
+import "./Assets/Fonts/static/Montserrat-ExtraBoldItalic.ttf";
+import "./Assets/Fonts/static/Montserrat-ExtraLight.ttf";
+import "./Assets/Fonts/static/Montserrat-ExtraLightItalic.ttf";
+import "./Assets/Fonts/static/Montserrat-Italic.ttf";
+import "./Assets/Fonts/static/Montserrat-Light.ttf";
+import "./Assets/Fonts/static/Montserrat-LightItalic.ttf";
+import "./Assets/Fonts/static/Montserrat-Medium.ttf";
+import "./Assets/Fonts/static/Montserrat-MediumItalic.ttf"; 
+import "./Assets/Fonts/static/Montserrat-Regular.ttf"; 
+import "./Assets/Fonts/static/Montserrat-SemiBold.ttf"; 
+import "./Assets/Fonts/static/Montserrat-SemiBoldItalic.ttf"; 
+import "./Assets/Fonts/static/Montserrat-Thin.ttf"; 
+import "./Assets/Fonts/static/Montserrat-ThinItalic.ttf"; 
+
+//import * as Fonts from "./Assets/Fonts/static";
+// import "./Assets/Fonts/static/Montserrat-Italic-VariableFont_wght.ttf";
+// import "./Assets/Fonts/static/Montserrat-VariableFont_wght.ttf"; 
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
